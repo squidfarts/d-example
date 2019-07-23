d42b7529dec73411b20c72130b3732c2f16e43ac
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 16:26:38 -0700

    Add files via upload

---
+++ src/main/main.d
@@ -0,0 +1,24 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.d                                                                    *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+package main;
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, Dlang' is desplayed to the standerd console.
+ *
+ */
+void main(string[] args) 
+{
+    writeLine("Hello, Dlang.");
+}// end of function main
